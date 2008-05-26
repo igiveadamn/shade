e875b1ee8234b838db634e176f1c214a27946d2a
igiveadamn <andy@igiveada.mn>
2008-05-26 22:46:01 +0000

    textmate project

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,107 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# Please install the Engine Yard Capistrano gem
+# gem install eycap --source http://gems.engineyard.com
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+require "eycap/recipes"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# =============================================================================
+# ENGINE YARD REQUIRED VARIABLES
+# =============================================================================
+# You must always specify the application and repository for every recipe. The
+# repository must be the URL of the repository you want this recipe to
+# correspond to. The :deploy_to variable must be the root of the application.
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :keep_releases,       5
+set :application,         "africa"
+set :repository,          "http://change.this.please"
+set :scm_username,       "username"
+set :scm_password,       "password"
+set :user,                "africa"
+set :password,            "muggOnod"
+set :deploy_to,           "/data/#{application}"
+
+
+set :deploy_via,          :filtered_remote_cache
+set :repository_cache,    "/var/cache/engineyard/#{application}"
+
+set :monit_group,         "africa"
+set :scm,                 :subversion
+set :runner,              "africa"
+
+
+set :production_database, "africa_production"
+set :production_dbhost,   "mysql50-1-master"
+
+
+
+
+
+
+
+
+
+set :dbuser,        "africa_db"
+set :dbpass,        "LitVihie"
+
+# comment out if it gives you trouble. newest net/ssh needs this set.
+ssh_options[:paranoid] = false
+
+
+# =============================================================================
+# ROLES
+# =============================================================================
+# You can define any number of roles, each of which contains any number of
+# machines. Roles might include such things as :web, or :app, or :db, defining
+# what the purpose of each machine is. You can also specify options that can
+# be used to single out a specific subset of boxes in a particular role, like
+# :primary => true.
+
+
+
+task :production do
+
+  role :web, "65.74.174.196:8475" # africa [mongrel] [mysql50-1-master]
+  role :app, "65.74.174.196:8475", :mongrel => true
+  role :db , "65.74.174.196:8475", :primary => true
+
+
+  set :rails_env, "production"
+  set :environment_database, defer { production_database }
+  set :environment_dbhost, defer { production_dbhost }
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# Any custom after tasks can go here.
+# after "deploy:symlink_configs", "africa_custom"
+# task :africa_custom, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
+#   run <<-CMD
+#   CMD
+# end
+# =============================================================================
+
+# Do not change below unless you know what you are doing!
+
+after "deploy", "deploy:cleanup"
+after "deploy:migrations" , "deploy:cleanup"
+after "deploy:update_code", "deploy:symlink_configs"
+
+# uncomment the following to have a database backup done before every migration
+# before "deploy:migrate", "db:dump"
+
