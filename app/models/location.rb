8ccd8945346b54572f072593077bedcce90f094b
igiveadamn <andy@igiveada.mn>
2008-05-27 01:00:54 +0000

    Implemented availabilty

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :last_updated_user, :class_name => 'User', :foreign_key => 'last_updated_user_id'
   has_many :requests
+  has_many :availabilities, :class_name => 'LocationAvailability', :order => 'updated_at'
+  
+  def availability
+    return 0 unless availabilities.any?
+    availabilities.last.availability
+  end
 end
--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -8,4 +8,10 @@ class LocationTest < Test::Unit::TestCase
     location.requests << Request.new(:description => 'need y')
     assert_equal 2, location.requests.size
   end
+  
+  def test_availability_returns_most_recent_location_availibility
+    location = Location.find(1)
+    assert_equal 25, location.availability
+  end
+  
 end
