af0641c84e700fce25c889d949ba504b5b0efc7b
igiveadamn <andy@igiveada.mn>
2008-05-30 10:13:38 +0000

    cleaned up the request index page, and hid the closed requests on the location page

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,7 @@ class Location < ActiveRecord::Base
   belongs_to :updated_by, :class_name => "User", :foreign_key => "updated_by"
   belongs_to :location_type
   
-  has_many :requests, :order => 'updated_at DESC'
+  has_many :requests, :order => 'updated_at DESC', :conditions => ["status <> 'closed'"]
   has_many :occupancies, :order => 'updated_at DESC' do
     def current
       find(:first)
