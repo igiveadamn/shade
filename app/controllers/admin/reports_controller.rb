14c5cba80fa3dccb38fe0e02c9af405311cc5d09
igiveadamn <andy@igiveada.mn>
2008-06-02 20:00:54 +0000

    removed unused layout

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -6,9 +6,7 @@ class Admin::ReportsController < Admin::ApplicationController
 
     for location in @locations
       @email_content += "#{location.id},\"#{ERB::Util.h(location.name)}\",\"#{ERB::Util.h(location.volunteer_summary)}\",\"#{ERB::Util.h(location.provisions_summary)}\"\r\n"
-      
     end
-
 
     respond_to do |format|
       format.html # index.html.erb
